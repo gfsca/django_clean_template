0ea3708d71a0998aed5dcf2fb15b28bf4c35849a
Gabriel Fonseca de Sa Cavalcanti de Albuquerque <gfsca@ug2c40.windows.cin.ufpe.br>
2018-06-12 10:17:40 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from templated_email import send_templated_mail #criar o arquivo de template do email
+
+def index(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        telefone = request.POST.get('telefone')
+        assunto = request.POST.get('assunto')
+        mensagem = request.POST.get('mensagem')
+        send_templated_mail(
+        		template_name='email',
+        		from_email= email,
+        		recipient_list=[''],
+        		context={
+            		'nome':nome,
+            		'telefone':telefone,
+            		'mensagem':mensagem,
+            		'assunto':assunto,
+            		'email':email,
+        		},
+			)
+
+        
+    return render(request, 'index.html',)
